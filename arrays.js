672302b55a8af2f1933b37d0480f6f053cb09efd
Jeremy Goldstein <jeremysports7@gmail.com>
2018-07-28 00:49:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(first, array){
-  console.log(array)
   console.log(first)
+  console.log(array)
   
 }
 addElementToBeginningOfArray("eminem", chocolateBars)
