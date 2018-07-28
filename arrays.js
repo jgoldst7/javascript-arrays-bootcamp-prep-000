780b5ea3f29b32e5e9ca78c66c2061add6e43bb8
Jeremy Goldstein <jeremysports7@gmail.com>
2018-07-28 00:09:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToBeginningOfArray("foo"){
+  return '${foo},...chocolateBars]'
+  
+}
+addElementToBeginningOfArray()
+
