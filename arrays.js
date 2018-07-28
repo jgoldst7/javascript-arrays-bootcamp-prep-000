ed8fdbc03af5018e16f83c73149d2be4716ffe1b
Jeremy Goldstein <jeremysports7@gmail.com>
2018-07-28 00:08:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,9 +4,10 @@ function addElementToBeginningOfArray("foo"){
   return '${foo},...chocolateBars]'
   
 }
+addElementToBeginningOfArray()
 
 function destructivelyAddElementToBeginningOfArray("foo"){
   return 'chocolateBars.unshift("${foo}")'
 
 }
-console.log(chocolateBars)
+destructivelyAddElementToBeginningOfArray()
